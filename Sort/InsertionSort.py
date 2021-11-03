8a2fe71f32fa3344a850a92b6be57a0bfccabc6d
Danmark_Jajang <54851789+Danmark-Jajang@users.noreply.github.com>
2021-11-04 00:53:50 +0900

    Update InsertionSort.py
    설명 변경

--- Sort/InsertionSort.py
+++ Sort/InsertionSort.py
@@ -1,9 +1,9 @@
-def InsertionSort(arr, n): #arr: array, n: size of array, 시작하자마자 가장 왼쪽 인덱스는 정렬되었다고 가정
-    for i in range(1, n):
-        loc = i-1 #last sorted position
+def InsertionSort(arr, n):  # arr: 무작위 배열, n: 배열의 길이
+    for i in range(1, n): # 1부터 시작하는 이유는 시작하자마자 가장 왼쪽 인덱스는 정렬되었다고 가정 / 2번째 부터 n번째 까지(index상으로 1부터 n-1까지)
+        loc = i-1 # 가장 최근 정렬된 위치, 처음에는 맨 앞 원소를 지정
         newItem = arr[i]
-        while(loc>=0 and newItem<arr[loc]):
-            arr[loc+1] = arr[loc]
+        while(loc>=0 and newItem<arr[loc]): # loc가 가장 앞으로 오거나 새로 삽입할 원소의 적합한 위치를 찾으면 while종료
+            arr[loc+1] = arr[loc]  # 기존 정렬된 원소들을 한칸씩 뒤로 당김
             loc=loc-1
         arr[loc+1] = newItem
     return arr
