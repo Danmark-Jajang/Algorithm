78552f035a3834303bd68a664a5b98aef3f018a4
Danmark_Jajang <54851789+Danmark-Jajang@users.noreply.github.com>
2021-11-04 00:53:42 +0900

    Update BubbleSort.py
    설명 변경

--- Sort/BubbleSort.py
+++ Sort/BubbleSort.py
@@ -1,6 +1,6 @@
-def BubbleSort(arr, n): #arr: array, n: size of array
-    for i in range(n-1, 0, -1):
-        for j in range(i):
+def BubbleSort(arr, n):  # arr: 무작위 배열, n: 배열의 길이
+    for i in range(n-1, 0, -1): # last
+        for j in range(i): 
             if(arr[j]>arr[j+1]):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
